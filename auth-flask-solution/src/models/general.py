b7d5641a09647be72052d796aaf63b896c54cae1
Elizaveta Tishkovets <etishkovec@ubic.tech>
2022-10-24 23:35:09 +0300

    Add user model

--- auth-flask-solution/src/models/general.py
+++ auth-flask-solution/src/models/general.py
@@ -1,9 +1,32 @@
+import enum
+import re
+import uuid
+
+from sqlalchemy import Enum
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import declared_attr
+
 from db.db_factory import get_db
 
 db = get_db()
 
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+class BaseMixin:
+    @declared_attr
+    def __tablename__(cls):
+        return re.sub(r'(?<!^)(?=[A-Z])', '_', cls.__name__.lower()).lower()
+
+
+class UUIDMixin(BaseMixin):
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+
+
+class RoleType(enum.Enum):
+    STANDARD = 'standard'
+    PRIVILEGED = 'privileged'
+
+
+class User(UUIDMixin, db.Model):
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     username = db.Column(db.String, unique=True, nullable=False)
-    email = db.Column(db.String)
+    role = db.Column(Enum(RoleType))
