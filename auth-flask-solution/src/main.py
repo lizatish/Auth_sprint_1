26f997ea4dac20c1652f279ca7f2059d9fe674b8
Elizaveta Tishkovets <etishkovec@ubic.tech>
2022-10-27 00:46:14 +0300

    Modify config, db using

--- auth-flask-solution/src/main.py
+++ auth-flask-solution/src/main.py
@@ -1,17 +1,9 @@
 from core.app_factory import create_app
-from db.db_factory import create_db
 
-# Инициализация приложения
-settings_filename = 'core.settings.DevSettings'
-app = create_app(settings_filename)
+from core.settings import get_settings
 
-# Инициализация БД
-create_db(app)
-
-# Регистрация отдельных компонентов (API)
-from api.v1.routes import api_v1
-
-app.register_blueprint(api_v1)
+settings = get_settings()
+app = create_app(settings)
 
 if __name__ == '__main__':
     app.run()
