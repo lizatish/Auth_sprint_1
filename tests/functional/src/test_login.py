924f658ff6e32636fd048021d1ecf60c68231598
Elizaveta Tishkovets <etishkovec@ubic.tech>
2022-10-30 21:40:24 +0300

    Add tests for login user

--- tests/functional/src/test_login.py
+++ tests/functional/src/test_login.py
@@ -1,23 +1,25 @@
 import pytest
+from flask.testing import FlaskClient
+from flask_sqlalchemy import SQLAlchemy
 from redis import Redis
 
 from tests.functional.settings import get_settings
-from tests.functional.testdata.login import test_data_for_success_login_user
+from tests.functional.testdata.login import test_data_for_success_login_user, test_data_for_not_exists_login_user, \
+    test_data_for_not_correct_password_login_user
 
 conf = get_settings()
 pytestmark = pytest.mark.asyncio
 
 
 @pytest.mark.parametrize(
-    'request_body, expected_body, expected_answer', test_data_for_success_login_user
+    'request_body, expected_answer', test_data_for_success_login_user
 )
 def test_success_login_user(
-        auth_api_client,
-        sqlalchemy_postgres,
+        auth_api_client: FlaskClient,
+        sqlalchemy_postgres: SQLAlchemy,
         sync_redis_pool: Redis,
         redis_flushall,
         request_body: dict,
-        expected_body: dict,
         expected_answer: dict,
 ):
     from models.db_models import User
@@ -34,9 +36,41 @@ def test_success_login_user(
     assert sync_redis_pool.get(f"refresh_{user.id}").decode('ascii') == response_body['refresh_token']
 
 
-def test_not_exists_login_user():
-    pass
+@pytest.mark.parametrize(
+    'request_body, expected_answer', test_data_for_not_exists_login_user
+)
+def test_not_exists_login_user(
+        auth_api_client: FlaskClient,
+        sqlalchemy_postgres: SQLAlchemy,
+        sync_redis_pool: Redis,
+        redis_flushall,
+        request_body: dict,
+        expected_answer: dict,
+):
+    from models.db_models import User
+
+    response = auth_api_client.post('/login', json=request_body)
+    response_body = response.json
+    user = User.query.filter_by(username=request_body['username']).first()
+
+    assert not user
+    assert response.status_code == expected_answer['status']
+    assert not response_body
 
 
-def test_not_correct_password_login_user():
-    pass
+@pytest.mark.parametrize(
+    'request_body, expected_answer', test_data_for_not_correct_password_login_user
+)
+def test_not_correct_password_login_user(
+        auth_api_client: FlaskClient,
+        sqlalchemy_postgres: SQLAlchemy,
+        sync_redis_pool: Redis,
+        redis_flushall,
+        request_body: dict,
+        expected_answer: dict,
+):
+    response = auth_api_client.post('/login', json=request_body)
+    response_body = response.json
+
+    assert response.status_code == expected_answer['status']
+    assert not response_body
--- tests/functional/testdata/login.py
+++ tests/functional/testdata/login.py
@@ -6,15 +6,64 @@ test_data_for_success_login_user = [
             "username": "ivan",
             "password": "ivanivanov"
         },
-        ['access_token', 'refresh_token'],
         {'status': HTTPStatus.OK},
     ),
-    # (
-    #     {
-    #         "username": "test",
-    #         "password": "qwe1Er23"
-    #     },
-    #     {'message': 'Successful registration'},
-    #     {'status': HTTPStatus.OK},
-    # ),
+    (
+        {
+            "username": "liza",
+            "password": "ivanivanov"
+        },
+        {'status': HTTPStatus.OK},
+    ),
+]
+
+test_data_for_unsuccess_login_user = [
+    (
+        {
+            "username": "ivanovivanov",
+            "password": "ivanivanov"
+        },
+        {'status': HTTPStatus.NOT_FOUND},
+    ),
+    (
+        {
+            "username": "lizaliza",
+            "password": "ivanivanov"
+        },
+        {'status': HTTPStatus.NOT_FOUND},
+    ),
+]
+
+test_data_for_not_exists_login_user = [
+    (
+        {
+            "username": "ivanovivanov",
+            "password": "ivanivanov"
+        },
+        {'status': HTTPStatus.NOT_FOUND},
+    ),
+    (
+        {
+            "username": "lizaliza",
+            "password": "ivanivanov"
+        },
+        {'status': HTTPStatus.NOT_FOUND},
+    ),
+]
+
+test_data_for_not_correct_password_login_user = [
+    (
+        {
+            "username": "ivan",
+            "password": "123456"
+        },
+        {'status': HTTPStatus.UNAUTHORIZED},
+    ),
+    (
+        {
+            "username": "liza",
+            "password": "lsKFSLkdmskdmfs"
+        },
+        {'status': HTTPStatus.UNAUTHORIZED},
+    ),
 ]
