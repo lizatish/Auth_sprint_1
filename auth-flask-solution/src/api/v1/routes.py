d9181de13b913622747fd53113f518cf5cb50f57
Oleg <oleg.rubtsov99@gmail.com>
2022-11-01 22:38:10 +0400

    finish task-7

--- auth-flask-solution/src/api/v1/routes.py
+++ auth-flask-solution/src/api/v1/routes.py
@@ -1,9 +1,10 @@
 from flask import Blueprint
 from flask_pydantic import validate
 
-from api.v1.schemas import UserLoginScheme, UserRegistration
+from api.v1.schemas import UserLoginScheme, UserRegistration, PasswordChange, UserData
 from services.auth import AuthService, get_auth_service
 from services.json import JsonService
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 api_v1 = Blueprint('api_v1', __name__)
 
@@ -31,3 +32,31 @@ def registration(body: UserRegistration):
         return JsonService.return_user_exists()
     get_auth_service().create_user(body.username, body.password)
     return JsonService.return_success_response(msg='Successful registration')
+
+
+@api_v1.route("/password-change", methods=["POST"])
+@validate()
+@jwt_required()
+def password_change(body: PasswordChange):
+    identity = get_jwt_identity()
+    user = AuthService.get_user_by_username(identity["username"])
+    if not user:
+        return JsonService.return_user_not_found()
+    if not user.check_password(body.old_password):
+        return JsonService.return_password_verification_failed()
+    get_auth_service().change_password(user, body.new_password)
+    return JsonService.return_success_response(msg='Successful password change')
+
+
+@api_v1.route("/user", methods=["PUT"])
+@validate()
+@jwt_required()
+def change_user_data(body: UserData):
+    identity = get_jwt_identity()
+    user = AuthService.get_user_by_username(identity["username"])
+    if not user:
+        return JsonService.return_user_not_found()
+    created = get_auth_service().change_user_data(user, body)
+    if not created:
+        return JsonService.return_user_exists()
+    return JsonService.return_success_response(msg='Successful user data changed')
--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -8,6 +8,7 @@ import pytest_asyncio
 from flask import Flask
 from flask.testing import FlaskClient
 from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import create_access_token
 from redis import Redis
 
 from core.app_factory import create_app
@@ -15,6 +16,7 @@ from db import redis
 from db.db_factory import get_db
 from tests.functional.settings import get_settings
 from tests.functional.testdata.postgresdata import users_data, roles_data
+from tests.functional.testdata.generate_tokens import users_data_for_tokens
 
 
 @pytest.fixture(scope="session")
@@ -63,6 +65,15 @@ def auth_api_client(app: Flask, sync_redis_pool: Redis) -> FlaskClient:
     """Фикстура апи-клиента."""
     redis.cache = sync_redis_pool
     yield app.test_client()
+
+
+@pytest.fixture(scope="session")
+def generate_access_token_for_user(app):
+    result = {}
+    with app.app_context():
+        for item in users_data_for_tokens:
+            result[item['username']] = create_access_token(identity=item)
+    yield result
 
 
 @pytest.fixture()
