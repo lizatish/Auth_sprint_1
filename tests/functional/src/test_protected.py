276d6d30bf0b61fba460cb50bb3349b9a6554848
Elizaveta Tishkovets <etishkovec@ubic.tech>
2022-11-02 22:48:57 +0300

    Base for admins routes

--- tests/functional/src/test_protected.py
+++ tests/functional/src/test_protected.py
@@ -4,7 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from redis import Redis
 
 from tests.functional.settings import get_settings
-from tests.functional.testdata.protected import test_data_protected_access
+from tests.functional.testdata.protected import test_data_protected_access, \
+    test_data_test_protected_admin_access_only_success, test_data_test_protected_admin_access_only_success_failed
 
 conf = get_settings()
 pytestmark = pytest.mark.asyncio
@@ -14,6 +15,50 @@ pytestmark = pytest.mark.asyncio
     'request_body, expected_answer, expected_body', test_data_protected_access
 )
 def test_protected_access(
+        auth_api_client: FlaskClient,
+        sqlalchemy_postgres: SQLAlchemy,
+        generate_access_token_for_user: dict,
+        sync_redis_pool: Redis,
+        request_body: dict,
+        expected_answer: dict,
+        expected_body: dict
+):
+    access_token = generate_access_token_for_user[request_body['username']]
+    headers = {'Authorization': f'Bearer {access_token}', 'content-type': 'application/json'}
+
+    response = auth_api_client.get('/protected', headers=headers)
+    response_body = response.json
+
+    assert response.status_code == expected_answer['status']
+    assert response_body == expected_body
+
+
+@pytest.mark.parametrize(
+    'request_body, expected_answer, expected_body', test_data_test_protected_admin_access_only_success
+)
+def test_protected_admin_access_only_success(
+        auth_api_client: FlaskClient,
+        sqlalchemy_postgres: SQLAlchemy,
+        generate_access_token_for_user: dict,
+        sync_redis_pool: Redis,
+        request_body: dict,
+        expected_answer: dict,
+        expected_body: dict
+):
+    access_token = generate_access_token_for_user[request_body['username']]
+    headers = {'Authorization': f'Bearer {access_token}', 'content-type': 'application/json'}
+
+    response = auth_api_client.get('/protected', headers=headers)
+    response_body = response.json
+
+    assert response.status_code == expected_answer['status']
+    assert response_body == expected_body
+
+
+@pytest.mark.parametrize(
+    'request_body, expected_answer, expected_body', test_data_test_protected_admin_access_only_success_failed
+)
+def test_protected_admin_access_only_failed(
         auth_api_client: FlaskClient,
         sqlalchemy_postgres: SQLAlchemy,
         generate_access_token_for_user: dict,
--- tests/functional/testdata/protected.py
+++ tests/functional/testdata/protected.py
@@ -20,3 +20,23 @@ test_data_protected_access = [
         {'msg': 'ADMIN'}
     )
 ]
+
+test_data_test_protected_admin_access_only_success = [
+    (
+        {
+            'username': 'admin',
+            'role': RoleType.ADMIN.name
+        },
+        {'status': HTTPStatus.OK},
+    )
+]
+test_data_test_protected_admin_access_only_success_failed = [
+    (
+        {
+            'username': 'ivan',
+            'role': RoleType.STANDARD.name
+        },
+        {'status': HTTPStatus.FORBIDDEN},
+        {"msg": "Admins only!"}
+    ),
+]
