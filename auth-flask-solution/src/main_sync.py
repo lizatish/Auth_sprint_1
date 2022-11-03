5d51b28c9817451b33d321bcf785aebbfb4d080f
Elizaveta Tishkovets <etishkovec@ubic.tech>
2022-11-03 22:38:52 +0300

    Cleaning

--- auth-flask-solution/src/main_sync.py
+++ auth-flask-solution/src/main_sync.py
@@ -1,4 +1,3 @@
-import aioredis
 from redis import Redis
 
 from core.app_factory import create_app
@@ -7,11 +6,6 @@ from db import redis
 
 settings = get_settings()
 app = create_app(settings)
-
-
-@app.route('/hello-world')
-def hello_world():
-    return 'Hello, World!'
 
 redis.cache = Redis(
     host=app.config['CACHE_HOST'],
